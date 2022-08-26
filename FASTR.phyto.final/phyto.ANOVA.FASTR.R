07318a997de661cdf8cda5df299922a0b86cb3a2
Flynn <Theodore.Flynn@water.ca.gov>
2022-08-26 11:32:24 -0700

    Update code to work with Git

--- FASTR.phyto.final/phyto.ANOVA.FASTR.R
+++ FASTR.phyto.final/phyto.ANOVA.FASTR.R
@@ -11,35 +11,38 @@ library("emmeans");packageVersion("emmeans")
 rm(list=ls()) #cleans workspace
 theme_set(theme_bw())
 
-setwd("C:/Users/tflynn/Documents/R/FASTR.phyto.redo")
+# Set working directory
+setwd("./FASTR.phyto.final/")
+getwd()
 
-load("yolo.phyto/phyto.BV.gen.RData")
-load("yolo.phyto/phyto.grp.sum.RData")
+## Load biovolume density data at group and genus level
+load("RData/phyto.gen.RData")
+load("RData/phyto.sum.RData")
 
-## Change Year category to factor so it won't 
-phyto.grp.sum$Year <- as.factor(phyto.grp.sum$Year)
-phyto.grp.sum$FlowPulseType <- as.factor(phyto.grp.sum$FlowPulseType)
+## Change Year category to factor
+phyto.sum$Year <- as.factor(phyto.sum$Year)
+phyto.sum$FlowPulseType <- as.factor(phyto.sum$FlowPulseType)
 
 ## Create QQ Plot to check for normality
-ggqqplot(log10(phyto.grp.sum$Total.BV.Density))
+ggqqplot(log10(phyto.sum$Total.BV.per.L))
 
 ## View histogram to check for normality
-hist(log10(phyto.grp.sum$Total.BV.Density))
+hist(log10(phyto.sum$Total.BV.per.L))
 
 ## Run Shapiro-Wilks test to check whether data is normally distributed
-shapiro.test(log10(phyto.grp.sum$Total.BV.Density))
+shapiro.test(log10(phyto.sum$Total.BV.per.L))
 
 ## Run Levene test to compare variance
-phyto.grp.sum %>% levene_test(log10(Total.BV.Density)~FlowPulseType) # p = 0.901
-phyto.grp.sum %>% levene_test(log10(Total.BV.Density)~Year) # p = 0.980
-phyto.grp.sum %>% levene_test(log10(Total.BV.Density)~Region) # p = 0.0230
+phyto.sum %>% levene_test(log10(Total.BV.per.L)~FlowPulseType) # p = 0.901
+phyto.sum %>% levene_test(log10(Total.BV.per.L)~Year) # p = 0.980
+phyto.sum %>% levene_test(log10(Total.BV.per.L)~Region) # p = 0.0230
 
 ## Run 3-way ANOVA
-phyto.grp.sum.aov <- phyto.grp.sum %>% anova_test(log10(Total.BV.Density) ~ Region*Year*ActionPhase)
-phyto.grp.sum.aov
+phyto.sum.aov <- phyto.sum %>% anova_test(log10(Total.BV.per.L) ~ Region*Year*ActionPhase)
+phyto.sum.aov
 
 ## Rosie Code
-L1 <- lm(log10(Total.BV.Density)~ Region*Year + Region*ActionPhase + ActionPhase*Year, data = phyto.grp.sum)
+L1 <- lm(log10(Total.BV.per.L)~ Region*Year + Region*ActionPhase + ActionPhase*Year, data = phyto.sum)
 
 summary(L1)
 
